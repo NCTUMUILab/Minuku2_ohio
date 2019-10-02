6f6748723ac368f76e4d85bcd5cc8b5b4044f91d
bufficecream <wbest601.cs05g@g2.nctu.edu.tw>
2019-10-02 17:56:28 +0800

    update to wave 14

--- app/src/main/java/edu/ohio/minuku_2/Receiver/WifiReceiver.java
+++ app/src/main/java/edu/ohio/minuku_2/Receiver/WifiReceiver.java
@@ -83,8 +83,8 @@ public class WifiReceiver extends BroadcastReceiver {
 
     //P.S. v1.0.4 is for v8
     //P.S. v2.0.4 is for v9
-    private String versionNumber = "v1.0.4";
-    private String wavesNumber = "13";
+    private String versionNumber = "v2.0.4";
+    private String wavesNumber = "14";
 
     public static final int HTTP_TIMEOUT = 10 * (int) Constants.MILLISECONDS_PER_SECOND;
     public static final int SOCKET_TIMEOUT = 20 * (int) Constants.MILLISECONDS_PER_SECOND;
